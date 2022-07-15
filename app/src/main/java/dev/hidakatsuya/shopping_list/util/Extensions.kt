196ac605713898941d9013b035ba01191fb2b6bb
Katsuya Hidaka <hidakatsuya@gmail.com>
2022-07-15 20:57:40 +0900

    remove unused code

--- app/src/main/java/dev/hidakatsuya/shopping_list/util/Extensions.kt
+++
@@ -1,9 +0,0 @@
-package dev.hidakatsuya.shopping_list.util
-
-import androidx.appcompat.widget.Toolbar
-import androidx.core.content.ContextCompat
-import dev.hidakatsuya.shopping_list.R
-
-fun Toolbar.displayBackButtonAsCloseIcon() {
-    navigationIcon = ContextCompat.getDrawable(context, R.drawable.ic_close)
-}
